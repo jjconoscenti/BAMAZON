285ad7d44faf5cf7f22869ae38205dd27660a786
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-05 21:20:04 -0500

    further experimentation

--- experiment.js
+++ experiment.js
@@ -2,6 +2,19 @@ var inquirer = require('inquirer');
 var colors = require('colors');
 var mysql = require('mysql');
 
+// global functions
+function showInventory() {
+    console.log("Show inventory");
+}
+
+function shopStore() {
+    console.log("Shop store");
+}
+
+function showCart() {
+    console.log("Show cart");
+}
+
 function menu(err, res) {
     if (err) {
         console.log(colors.red('Error found: ' + err));
@@ -10,32 +23,44 @@ function menu(err, res) {
             name: 'customerSelection',
             type: 'list',
             message: 'Welcome to Bamazon, what would you like to do?',
-            choices: ["View store items", "Shop", "View Cart"]
+            choices: ["Shop", "View Cart", "Exit"]
         }]).then(function(answer) {
             var waitMsg;
 
             switch (answer.customerSelection) {
 
-                case "View Store Items":
-                    console.log(colors.green("Here's what we have available"));
-                    waitMsg = setTimeout(showInventory, 3000);
-                    break;
+                // case "View Store Items":
+                //     console.log(colors.green("View store items"));
+                //     waitMsg = setTimeout(showInventory, 2000);
+                //     break;
 
                 case "Shop":
-                    console.log(colors.green("Shop tree"));
-                    waitMsg = setTimeout(shopStore, 3000);
+                    console.log(colors.green("============================="));
+                    console.log(colors.white("Here's what we have available"));
+                    console.log(colors.green("============================="));
+                    console.log(colors.white("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.blue("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.white("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.blue("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.white("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.blue("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.white("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.blue("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.white("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    console.log(colors.blue("item1 id " + "|" + " item1 name " + "|" + " item1 price " + "|" + " item1 stockqty"));
+                    waitMsg = setTimeout(shopStore, 2000);
                     break;
 
                 case "View Cart":
                     console.log(colors.green("Here's your cart"));
-                    waitMsg = setTimeout(showCart, 3000);
+                    waitMsg = setTimeout(showCart, 2000);
+
+                case 'Exit':
+                    console.log(colors.yellow("Thanks for shopping with Bamazon!"));
+                    break;
             }
         })
     }
 }
 
-function showInventory();
-
-function shopStore();
-
-function showCart();
+menu()
